bd306f9badb206e6197f7d0a2dd86740246cb2bd
Andre M Germain <caluwen@burstflare.com>
2026-03-22 00:11:37 -0400

    Restored controls

--- src/HelloMurder/HelloMurderGame.cs
+++ src/HelloMurder/HelloMurderGame.cs
@@ -1,6 +1,10 @@
 ﻿using Bang;
+using HelloMurder.Core.Input;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Murder;
+using Murder.Core.Geometry;
+using Murder.Core.Graphics;
 using Murder.Core.Input;
 using Murder.Serialization;
 using System.Text.Json;
@@ -21,8 +25,35 @@ public class HelloMurderGame : IMurderGame
 	
     public ComponentsLookup ComponentsLookup { get; } = new MurderComponentsLookup();
 
+    public RenderContext CreateRenderContext(GraphicsDevice graphicsDevice,
+                                             Camera2D camera,
+                                             RenderContextFlags settings)
+    {
+        RenderContext renderContext = new(graphicsDevice, camera, settings);
+
+        WindowChangeSettings windowChangeSettings = new(
+            new Point(Game.GraphicsDevice.Viewport.Width, Game.GraphicsDevice.Viewport.Height)
+        )
+        {
+            ScalingKind = Murder.Save.ScalingKind.Snap
+        };
+
+        renderContext.OnClientWindowChanged(windowChangeSettings);
+
+        return renderContext;
+    }
+
     public void Initialize()
     {
+        Game.Input.RegisterAxes(InputAxis.Movement, [
+            GamepadAxis.LeftThumb,
+            GamepadAxis.Dpad,
+        ]);
+
+        Game.Input.Register(InputAxis.Ui,
+            new InputButtonAxis(Keys.W, Keys.A, Keys.S, Keys.D),
+            new InputButtonAxis(Keys.Up, Keys.Left, Keys.Down, Keys.Right));
+
         Game.Input.RegisterButton(MurderInputButtons.Submit, Keys.Space);
         Game.Input.RegisterButton(MurderInputButtons.Submit, Keys.Enter);
         Game.Input.RegisterButton(MurderInputButtons.Submit, Buttons.A);
